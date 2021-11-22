1cd05901405853f0e937a13af6242b820ef086d8
donghyun's mac mini <donghyun-kim@donghyunsui-Macmini.local>
2021-11-22 17:15:18 +0900

    navigation view 추가

--- app/src/main/java/com/example/easycarpoolapp/MainActivity.kt
+++ app/src/main/java/com/example/easycarpoolapp/MainActivity.kt
@@ -2,19 +2,51 @@ package com.example.easycarpoolapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
 import com.example.easycarpoolapp.databinding.ActivityInfoBinding
+import com.example.easycarpoolapp.databinding.ActivityMainBinding
+import com.example.easycarpoolapp.navigation.NavigationViewManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationViewManager.Callback {
 
-
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var actionBar : ActionBar
+    private lateinit var navigationViewManager: NavigationViewManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setActionBar()
+        navigationViewManager = NavigationViewManager(this, binding)
+        navigationViewManager.setNavView()
 
     }
+    private fun setActionBar(){
+        setSupportActionBar(binding.appBarMain.toolbar)
+        actionBar = supportActionBar!!
+        actionBar.setDisplayShowCustomEnabled(false)    //기존의 title지우기
+        actionBar.setDisplayHomeAsUpEnabled(true)   //뒤로가기 버튼이 생기게 하기 위해 True설정
+        actionBar.setHomeAsUpIndicator(R.drawable.icon_menu_delete)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home ->{   //home키 - toolbar에 추가된 외쪽 상단의 버튼을 의미 따로 id를 지정하지 않음
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onLogoutSelected() {
+        TODO("Not yet implemented")
+    }
 }
+
+
---
+++ app/src/main/java/com/example/easycarpoolapp/navigation/NavigationViewManager.kt
@@ -0,0 +1,58 @@
+package com.example.easycarpoolapp.navigation
+
+import android.content.Context
+import android.content.Intent
+import android.service.autofill.UserData
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.example.easycarpoolapp.R
+import com.example.easycarpoolapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class NavigationViewManager (val context : Context, val binding : ActivityMainBinding) : NavigationView.OnNavigationItemSelectedListener{
+
+    interface Callback{
+        fun onLogoutSelected()
+    }
+
+
+    private var callback : Callback? = null
+
+    init{
+        callback = context as Callback
+    }
+
+
+
+
+    public fun setNavView(){
+
+        val btnLogState : TextView = binding.navView.getHeaderView(0).findViewById(R.id.navLogStateBtn)
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        btnLogState.setOnClickListener {
+            Toast.makeText(context, "logout action prototype", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_test1 ->{
+                Toast.makeText(context, "prototype1", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.nav_test2->{
+                Toast.makeText(context, "prototype1", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+        }
+        return true
+    }
+
+
+
+}
