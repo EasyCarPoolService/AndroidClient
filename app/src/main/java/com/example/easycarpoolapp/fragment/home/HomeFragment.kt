853f7fcaa8fd20707e444d60f597df42705896ef
DHKim <dhkim0405@naver.com>
2021-12-25 16:23:57 +0900

    게시판 layout구성

--- app/src/main/java/com/example/easycarpoolapp/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/easycarpoolapp/fragment/home/HomeFragment.kt
@@ -27,6 +27,8 @@ class HomeFragment(): Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
